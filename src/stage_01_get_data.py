e85db8841cbfc14a5a453f08b13c64b4e24e3055
atulkumarrai <iamatul1214@gmail.com>
2022-03-02 23:12:44 +0530

    condition for extraction added

--- src/stage_01_get_data.py
+++ src/stage_01_get_data.py
@@ -42,8 +42,11 @@ def main(config_path):
 
     ## unzip operation
     unzip_data_dir=config["data"]["unzip_data_dir"]
-    create_directories([unzip_data_dir])
-    unzip_file(source=data_file_path, dest=unzip_data_dir)
+    if not os.path.exists(unzip_data_dir):
+        create_directories([unzip_data_dir])
+        unzip_file(source=data_file_path, dest=unzip_data_dir)
+    else:
+        logging.info("data extracted successfully")
 
 
 if __name__ == '__main__':
